6f1748ba80313ed63cd09afb9606d06f1c5cc07c
Softwarica College Lab <softwarica@softwarica.com>
2019-05-02 12:10:49 +0545

    insert in sqlite

--- app/src/main/java/com/sqlite/MainActivity.java
+++ app/src/main/java/com/sqlite/MainActivity.java
@@ -9,31 +9,37 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etWord, etMeaning;
+    Button btnAddWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final EditText etWord=findViewById(R.id.etWord);
-        final EditText etMeaning=findViewById(R.id.etMeaning);
-        Button btnAddWord=findViewById(R.id.btnAddWord);
+        etWord = findViewById(R.id.etWord);
+        etMeaning = findViewById(R.id.etMeaning);
+        btnAddWord = findViewById(R.id.btnAddWord);
 
-        final MyHelper myHelper=new MyHelper(this);
-        final SQLiteDatabase sqLiteDatabase=myHelper.getWritableDatabase();
+
+        final MyHelper myHelper = new MyHelper(this);
+        final SQLiteDatabase sqLiteDatabase = myHelper.getWritableDatabase();
 
         btnAddWord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(myHelper.InsertData(etWord.getText().toString(),etMeaning.getText().toString(),sqLiteDatabase))
-                {
-                    Toast.makeText(MainActivity.this,"succesafull", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
+                long id = myHelper.InsertData(etWord.getText().toString(), etMeaning.getText().toString(), sqLiteDatabase);
+                if (id > 0) {
+                    Toast.makeText(MainActivity.this, "successful", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
     }
+
+
+
+
 }
--- app/src/main/java/com/sqlite/MyHelper.java
+++ app/src/main/java/com/sqlite/MyHelper.java
@@ -1,14 +1,23 @@
 package com.sqlite;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import java.security.Key;
+
 public class MyHelper extends SQLiteOpenHelper {
 
     private static  final String databaseName="DictionaryDB";
     private static final int dbVersion=1;
+
+    private static final String tblWord="tblWord";
+    private static final String WordID="wordId";
+    private static final String Word="Word";
+    private static final String Meaning="Meaning";
+
 
     public MyHelper(Context context) {
         super(context, databaseName, null, dbVersion);
@@ -16,12 +25,11 @@ public class MyHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-     String query="CREATE TABLE WORDS"
-        +"("+
-             "WordID INTEGER PRIMARY KEY AUTOINCREMENT,"+
-             "Word TEXT,"+
-             "Meaning TEXT)";
-
+     String query="CREATE TABLE " + tblWord +
+                    "(" + WordID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    Word + " TEXT,"
+                    + Meaning+ " TEXT " +
+                    ")";
      db.execSQL(query);
     }
 
@@ -30,15 +38,12 @@ public class MyHelper extends SQLiteOpenHelper {
 
     }
 
-    public boolean InsertData(String word, String meaning, SQLiteDatabase db){
-        try {
-            String query="insert into Words(Word, Meaning) values('"+word+"','"+meaning+"')";
-            db.execSQL(query);
-            return true;
-
-        }catch (Exception e){
-            Log.d("Error: ",e.toString());
-            return false;
-        }
+    public Long InsertData(String word, String meaning, SQLiteDatabase db){
+     Long id;
+        ContentValues contentValues=new ContentValues();
+        contentValues.put("WordTEXT",word);
+        contentValues.put("Meaning", meaning);
+        id=db.insert(tblWord, null, contentValues);
+        return id;
     }
 }
